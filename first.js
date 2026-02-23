ce41cf880467b71ae6e2ae44a37f69c367e3dc70
alina hashmi <heybbyali@gmail.com>
2026-02-24 00:00:08 +0530

    uploded first practice code of js

---
+++ first.js
@@ -0,0 +1,26 @@
+console.log("hey alina!");
+console.log("this is my first js!");
+alert("welcome alina");
+alert("congratulations!!");
+age = 24 
+name = "arif";
+console.log(name)
+console.log(age + 10);
+x = null;
+console.log(x);
+y = undefined;
+console.log(y);
+isfollow = false
+console.log(isfollow)
+var age = 10;
+var age = 20;
+console.log(age);
+{
+    let a = 20; 
+    let b = 30; 
+    console.log(a)
+console.log(b)}
+a = 10; 
+console.log(a)
+const pi = 3.14;
+console.log(pi)
